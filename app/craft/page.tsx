e21477ccabdd38ef41f117b7b6f120c9d3765ac6
Ankit Sharma <ankitsharma2615@gmail.com>
2026-01-04 23:15:38 +0530

    feat: add craft layout

--- app/craft/page.tsx
+++ app/craft/page.tsx
@@ -1,8 +1,17 @@
+import { Highlighter } from "@/components/ui/highlighter";
+
 const page = () => {
   return (
-    <div>
-      <h1 className="text-2xl">This is Craft Page</h1>
-    </div>
+    <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="my-12">
+        <Highlighter action="underline" color="#ff4d6d">
+          <h2 className="font-doto text-2xl font-semibold">Craft </h2>
+        </Highlighter>
+        <span className="font-inconsolata text-sm text-muted-foreground/80 font-medium ml-4">
+          My experiments & free time wandering
+        </span>
+      </div>
+    </main>
   );
 };
 
