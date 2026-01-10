1021d72d603a448712542c6d0f68ce5d2b054ea2
Ankit Sharma <ankitsharma2615@gmail.com>
2026-01-10 22:29:20 +0530

    revert: Navbar & chore: remove dependencies

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,84 +1,7 @@
-// "use client";
-
-// import Link from "next/link";
-// import { useState } from "react";
-// import {
-//   Feather,
-//   FlaskConical,
-//   Folder,
-//   GraduationCap,
-//   Home,
-//   LucideIcon,
-//   Menu,
-// } from "lucide-react";
-// import { usePathname } from "next/navigation";
-
-// import { cn } from "@/lib/utils";
-// import { Button } from "@/components/ui/button";
-// import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-// import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
-
-// interface NavItem {
-//   name: string;
-//   href: string;
-//   icon: LucideIcon;
-// }
-
-// const navItems: NavItem[] = [
-//   { name: "Home", href: "/", icon: Home },
-//   { name: "Work", href: "/work", icon: GraduationCap },
-//   { name: "Projects", href: "/projects", icon: Folder },
-//   { name: "Lab", href: "/lab", icon: FlaskConical },
-//   { name: "Skills", href: "/skills", icon: Feather },
-// ];
-
-// const Navbar = () => {
-//   const [isOpen, setIsOpen] = useState<boolean>(false);
-//   const pathname = usePathname();
-
-//   return (
-//     <header>
-//       <nav className="max-w-max mx-auto mt-4 font-inconsolata border rounded-full">
-//         <div className="flex justify-center items-center">
-//           <div className="flex items-center divide-x-2">
-//             {navItems.map((item) => {
-//               const isActive =
-//                 (pathname === "/" && item.href === "/") ||
-//                 pathname === item.href ||
-//                 pathname.startsWith(`${item.href}/`);
-
-//               return (
-//                 <Link
-//                   key={item.name}
-//                   href={item.href}
-//                   className={cn(
-//                     "px-2.5 md:px-5 py-4",
-//                     isActive && "bg-foreground text-background rounded-full"
-//                   )}
-//                 >
-//                   <span className="flex items-center gap-2 text-base font-medium">
-//                     <item.icon className="size-4" /> {item.name}
-//                   </span>
-//                 </Link>
-//               );
-//             })}
-
-//             <span className="px-2.5 md:px-5 mt-2">
-//               <AnimatedThemeToggler className="h-6 w-6 cursor-pointer" />
-//             </span>
-//           </div>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Navbar;
-
 "use client";
 
-import React from "react";
-import { FloatingDock } from "@/components/ui/floating-dock";
+import Link from "next/link";
+import { useState } from "react";
 import {
   Feather,
   FlaskConical,
@@ -88,49 +11,110 @@ import {
   LucideIcon,
   Menu,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
+
+interface NavItem {
+  name: string;
+  href: string;
+  icon: LucideIcon;
+}
+
+const navItems: NavItem[] = [
+  { name: "Home", href: "/", icon: Home },
+  { name: "Work", href: "/work", icon: GraduationCap },
+  { name: "Projects", href: "/projects", icon: Folder },
+  { name: "Lab", href: "/lab", icon: FlaskConical },
+  { name: "Skills", href: "/skills", icon: Feather },
+];
 
 export const Navbar = () => {
-  const links = [
-    {
-      title: "Home",
-      icon: (
-        <Home className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "/",
-    },
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const pathname = usePathname();
 
-    {
-      title: "Work",
-      icon: (
-        <GraduationCap className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "/work",
-    },
-    {
-      title: "Projects",
-      icon: (
-        <Folder className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "/projects",
-    },
-    {
-      title: "Lab",
-      icon: (
-        <FlaskConical className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "/lab",
-    },
-    {
-      title: "Skills",
-      icon: (
-        <Feather className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "/skills",
-    },
-  ];
   return (
-    <div className="flex items-center justify-center w-full mt-4">
-      <FloatingDock items={links} />
-    </div>
+    <header>
+      <nav className="font-inconsolata border-b-2 border-dashed">
+        <div className="flex justify-between items-center">
+          <Link
+            href="/"
+            className="px-2.5 md:px-5 py-4 border-r-2 border-dashed bg-background"
+          >
+            <span className="text-base text-foreground font-medium select-none">
+              ANKIT SHARMA.
+            </span>
+          </Link>
+          <div className="hidden lg:flex items-center divide-x-2 divide-dashed">
+            {navItems.map((item) => {
+              const isActive =
+                (pathname === "/" && item.href === "/") ||
+                pathname === item.href ||
+                pathname.startsWith(`${item.href}/`);
+
+              return (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={cn(
+                    "px-2.5 md:px-5 py-4",
+                    isActive && "bg-foreground text-background"
+                  )}
+                >
+                  <span className="flex items-center gap-2 text-base font-medium">
+                    <item.icon className="size-4" /> {item.name}
+                  </span>
+                </Link>
+              );
+            })}
+
+            <span className="px-2.5 md:px-5 mt-2">
+              <AnimatedThemeToggler className="h-6 w-6 cursor-pointer" />
+            </span>
+          </div>
+
+          <div className="lg:hidden flex items-center gap-2">
+            <AnimatedThemeToggler className="h-6 w-6 cursor-pointer" />
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="px-0 text-lg hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 cursor-pointer"
+                >
+                  <Menu className="h-6 w-6" />
+                  <span className="sr-only">Toggle Menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="bg-background" side="top">
+                <div className="flex flex-col px-6 divide-y-2 divide-dashed">
+                  {navItems.map((item) => {
+                    const isActive =
+                      (pathname === "/" && item.href === "/") ||
+                      pathname === item.href ||
+                      pathname.startsWith(`${item.href}/`);
+                    return (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={cn(
+                          "px-2.5 md:px-5 py-4 text-xl text-foreground",
+                          isActive && "bg-background text-foreground"
+                        )}
+                        onClick={() => setIsOpen(false)}
+                      >
+                        {item.name}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
