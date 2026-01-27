94fa740d6b52f691206c7e7fa1e705948d58bec1
Ankit Sharma <ankitsharma2615@gmail.com>
2026-01-27 20:41:20 +0530

    chore: remove nextra

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+import { Heading } from "@/components/common/Heading";
+
+const page = () => {
+  return (
+    <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Heading
+        title="Blog"
+        subTitle="Animations and components built with code."
+      />
+    </main>
+  );
+};
+
+export default page;
