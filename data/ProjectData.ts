b8afdf886d4c4aa87f64149dd2fb9ba24d213838
Ankit Sharma <ankitsharma2615@gmail.com>
2026-02-16 22:43:23 +0530

    hide dummy project

--- data/ProjectData.ts
+++ data/ProjectData.ts
@@ -19,17 +19,17 @@ export const projectData: ProjectItem[] = [
     image: "/lab/process-pillars.png",
     link: "https://mentra.gg",
   },
-  {
-    name: "School Management System",
-    description:
-      "Designed and developed a comprehensive school management system with features for student records, attendance tracking, and grade management.",
-    achievements: [
-      "Implemented a robust backend using Node.js and Express.",
-      "Designed and implemented a responsive frontend with React and TypeScript.",
-      "Integrated database management with PostgreSQL for efficient data handling.",
-      "Implemented user authentication and role-based access control for secure data management.",
-    ],
-    image: "/projects/image.png",
-    link: "https://mentra.gg",
-  },
+  // {
+  //   name: "School Management System",
+  //   description:
+  //     "Designed and developed a comprehensive school management system with features for student records, attendance tracking, and grade management.",
+  //   achievements: [
+  //     "Implemented a robust backend using Node.js and Express.",
+  //     "Designed and implemented a responsive frontend with React and TypeScript.",
+  //     "Integrated database management with PostgreSQL for efficient data handling.",
+  //     "Implemented user authentication and role-based access control for secure data management.",
+  //   ],
+  //   image: "/projects/image.png",
+  //   link: "https://mentra.gg",
+  // },
 ];
